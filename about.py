c406ca725adbd8da296ebd9451aa13c4bf8bef50
b3!ngD3v <hiitech.ml@gmail.com>
2020-07-27 16:37:02 +0530

    mixed

---
+++ about.py
@@ -0,0 +1,34 @@
+def about():
+    print('''
+    
+    All Bombs away Sir
+      \
+           \      Goodbye Dullsville!
+                __|__  /
+       .'(\      .-.     /)'.
+    +-====(*)===: " :===(o)=====-+
+            \).  '-'  .(/                            
+                 +=
+                 +=
+                 +=           █▄▄ █▀█ █▀▄▀█ █▄▄ █▀▀ █▀█ ▀█▀ █ █ █▀█ █▄ █
+                              █▄█ █▄█ █ ▀ █ █▄█ ██▄ █▀▄  █  █▀█ █▄█ █ ▀█
+                 += 
+                 +=          ###########################################
+                 +=          # Version >>> v1.1 (stable)               #
+                             # Last Update >>> 29th July 2020          #
+                 +=          # Coded by b3!ngD3v (Sinha)               #
+                 +=          # Named by Ritik Gupta                    #
+                 +=          # GitHub >>> https://github.com/b31ngd3v  #
+                 +=          ###########################################
+''')
+
+def menu():
+     print('''
+    |-PRESS---------------|
+    | 1] Call Bomber      |
+    | 2] SMS Bomber       |
+    | 3] Instagram Bomber |
+    | 4] WhatsApp Bomber  |
+    | 5] About            |
+    | 6] Exit Script      |
+    |---------------------|''')
