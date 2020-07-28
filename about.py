7a2fc9eba5cc228fe2e33d954131dd2e253c59a0
b3!ngD3v <contact.pycoder@gmail.com>
2020-07-29 00:49:25 +0530

    Update about.py

--- about.py
+++ about.py
@@ -14,21 +14,10 @@ def about():
                               █▄█ █▄█ █ ▀ █ █▄█ ██▄ █▀▄  █  █▀█ █▄█ █ ▀█
                  += 
                  +=          ###########################################
-                 +=          # Version >>> v1.1 (stable)               #
-                             # Last Update >>> 29th July 2020          #
+                 +=          # Version >>> v1.0 (Beta)                 #
+                             # Last Update >>> 11th July 2020          #
                  +=          # Coded by b3!ngD3v (Sinha)               #
                  +=          # Named by Ritik Gupta                    #
                  +=          # GitHub >>> https://github.com/b31ngd3v  #
                  +=          ###########################################
 ''')
-
-def menu():
-     print('''
-    |-PRESS---------------|
-    | 1] Call Bomber      |
-    | 2] SMS Bomber       |
-    | 3] Instagram Bomber |
-    | 4] WhatsApp Bomber  |
-    | 5] About            |
-    | 6] Exit Script      |
-    |---------------------|''')
